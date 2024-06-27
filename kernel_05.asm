08d934f0b31a85017bef28c92ba06402a3c74c29
Diego Alberti <developer.viset@gmail.com>
2024-06-26 23:27:33 -0300

    feat: first bytebeat in kernel05

--- kernel_05.asm
+++ kernel_05.asm
@@ -15,15 +15,27 @@ temp            ds 1
                 seg.u	vars		; uninitialized segment
                 org	$90             ; origin set at base of ram
 
-my16bit_lsb     ds 1                ; 1 byte - 16 bit counter lsb
-my16bit_msb     ds 1                ; 1 byte - 16 bit counter msb
+c16_1     ds 3                ; 1 byte - 16 bit counter lsb
+
 scanline        ds 1                ; 1 byte - current scanline
 fcount          ds 1                ; 1 byte - frame counter
+t_               ds 2                ; 1 byte - temp
+mod_1           ds 1                ; 1 byte - modulo 1
 
                 seg	main    		; start of main segment
                 org $F000
 
 reset:			CLEAN_START			; ouput: all ram registers 0
+
+                lda #1
+                sta CTRLPF
+
+                lda #0	            ; (2) set D1 to 0 to end VBLANK
+                sta c16_1,1
+                sta c16_1
+                
+                lda #$1E
+                sta COLUPF          
 
 nextframe:		VERTICAL_SYNC	    ; output: a = 0; 3 scanlines
 ; -------- set timer -------------------------------
@@ -32,10 +44,11 @@ nextframe:		VERTICAL_SYNC	    ; output: a = 0; 3 scanlines
                 lda #44             ; We'll round down, and clean up the remaining cycles with a WSYNC
                 sta TIM64T          ; Set a count of 43 with 64-clock interval
 ; -------- do stuff  -------------------------------
-
-                jsr add
-                jsr add
-                jsr add
+                ; jsr add
+                ; jsr add
+                ; jsr add
+                ; jsr add
+                jsr inc16_1
 
 ; -------- wait ------------------------------------                                
                 lda INTIM           ; check the timer          
@@ -43,38 +56,33 @@ nextframe:		VERTICAL_SYNC	    ; output: a = 0; 3 scanlines
 ; -------- done ------------------------------------
                 
                 lda #0
-                sta WSYNC           ; primera linea visible
+                
                 sta VBLANK
                 
                 
                 lda #PF_H
                 sta scanline       ; 1 byte - current scanline
+                
+                
+                
+                sta WSYNC           ; primera linea visible
+                lda c16_1,1
+                sta COLUPF
+kernel:		    
+                sta WSYNC           ; no lo cuento en la snl  
+                
+                
+                
+                jsr bytebeat2
 
-kernel:		    sta WSYNC           ; no lo cuento en la snl       		            
+                lda t_                
+                sta PF1
 
-                
-                ; lda scanline
-                ; cmp my16bit_lsb
-                
-                lda scanline
-                adc my16bit_lsb
-                sta COLUBK          ;(3)
-                    sleep 6
+                lda t_,1                
+                sta PF2
 
-                sbc #$20 
-                sta COLUBK          ;(3)
-                    sleep 6
-                
-                sbc #$20 
-                sta COLUBK          ;(3)
-                    sleep 6
-                
-                sbc #$20 
-                sta COLUBK          ;(3)
-                    sleep 6
-                
-                sbc #$0f 
-                sta COLUBK          ;(3)
+                ; lda c16_1,1
+                ; sta COLUBK          ;(3)
                 
                 dec scanline                 ; (2)
                 bne kernel          ; (3) 2 bytes del opcode (beq) + 1 byte operando + byte del salto
@@ -100,15 +108,55 @@ kernel:		    sta WSYNC           ; no lo cuento en la snl
                 
                 jmp nextframe       ; (3) jump back up to start the next frame
 
-add	            clc		
-                lda my16bit_lsb
+inc16_1	        clc		
+                lda c16_1,1
                 adc #1
-                sta my16bit_lsb
+                sta c16_1,1
                 bcc ok             
-                inc my16bit_msb
+                inc c16_1
 ok
                 rts
 
+
+bytebeat2       clc
+                lda c16_1,1 
+                adc scanline
+                bcc next
+                inc c16_1
+next                
+                lsr t_,1    
+                ; ora t_,1
+                sta t_,1
+                ; asl t_,1
+                ; ora t_,1           
+                
+                
+                lsr t_    
+                ; ora t_
+                sta t_
+                ; asl t_
+                ; ora t_           
+                
+                ; ror t_                
+                
+                inc t_
+                
+                rts
+
+
+bytebeat
+                lda t_
+                ror
+                ror
+                ora t_
+                ror
+                ror
+                ora t_
+                and #$3F
+                ror
+                ror
+                and t_
+                rts
 
                 org 	$FFFA
 
