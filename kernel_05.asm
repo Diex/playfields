de24b85f4be3641da344bb844a0802582fe5f518
Diego Alberti <developer.viset@gmail.com>
2024-06-27 14:33:26 -0300

    dev: added new params address block

--- kernel_05.asm
+++ kernel_05.asm
@@ -10,18 +10,21 @@ PF_H            equ 192            ; playfield height
                 
                 seg.u	temp		; uninitialized segment
                 org	$80             ; origin set at base of ram
-                ; 16 bytes of uninitialized memory
+                                    ; up to 9F
+params          ds 16
+c16_1           ds 3  
+MEM             ds 2
+                
+                seg.u	vars		
+                org	$A0             
+
 temp            ds 1                
-                seg.u	vars		; uninitialized segment
-                org	$90             ; origin set at base of ram
-
-c16_1     ds 3                ; 1 byte - 16 bit counter lsb
-
 scanline        ds 1                ; 1 byte - current scanline
 fcount          ds 1                ; 1 byte - frame counter
-t_               ds 2                ; 1 byte - temp
+t_              ds 2                ; 1 byte - temp
 mod_1           ds 1                ; 1 byte - modulo 1
 
+                
                 seg	main    		; start of main segment
                 org $F000
 
@@ -30,9 +33,9 @@ reset:			CLEAN_START			; ouput: all ram registers 0
                 lda #1
                 sta CTRLPF
 
-                lda #0	            ; (2) set D1 to 0 to end VBLANK
-                sta c16_1,1
-                sta c16_1
+                ; lda #0	            ; (2) set D1 to 0 to end VBLANK
+                ; sta c16_1,1
+                ; sta c16_1
                 
                 lda #$1E
                 sta COLUPF          
@@ -71,16 +74,19 @@ nextframe:		VERTICAL_SYNC	    ; output: a = 0; 3 scanlines
 kernel:		    
                 sta WSYNC           ; no lo cuento en la snl  
                 
-                
-                
-                jsr bytebeat2
-
                 lda t_                
                 sta PF1
 
                 lda t_,1                
                 sta PF2
 
+                
+                sta WSYNC
+
+                jsr bytebeat2
+                dec scanline
+
+                
                 ; lda c16_1,1
                 ; sta COLUBK          ;(3)
                 
@@ -108,6 +114,16 @@ kernel:
                 
                 jmp nextframe       ; (3) jump back up to start the next frame
 
+
+
+
+                ; Shift a 16bit value by one place left (e.g. multiply by two)
+asl16       ASL MEM+0       ;Shift the LSB
+            ROL MEM+1       ;Rotate the MSB
+            rts
+
+
+
 inc16_1	        clc		
                 lda c16_1,1
                 adc #1
@@ -120,19 +136,19 @@ ok
 
 bytebeat2       clc
                 lda c16_1,1 
-                adc scanline
-                bcc next
-                inc c16_1
-next                
+                ; adc scanline
+                ; bcc next
+                ; inc c16_1
+; next                
                 lsr t_,1    
-                ; ora t_,1
+                ora t_,1
                 sta t_,1
                 ; asl t_,1
                 ; ora t_,1           
                 
-                
+                lda c16_1 
                 lsr t_    
-                ; ora t_
+                ora t_
                 sta t_
                 ; asl t_
                 ; ora t_           
