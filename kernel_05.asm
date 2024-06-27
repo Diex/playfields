6e40d673fafe8d532c4923222e723b340a04a2ba
Diego Alberti <developer.viset@gmail.com>
2024-06-27 17:39:35 -0300

    feat: properly shows count - PF2 reflected

--- kernel_05.asm
+++ kernel_05.asm
@@ -11,10 +11,13 @@ PF_H            equ 192            ; playfield height
                 seg.u	temp		; uninitialized segment
                 org	$80             ; origin set at base of ram
                                     ; up to 9F
-params          ds 16
-c16_1           ds 3  
+params          ds 8
+c16_1           ds 2
+plfys           ds 3
+revbits         ds 2
+
 MEM             ds 2
-                
+
                 seg.u	vars		
                 org	$A0             
 
@@ -33,10 +36,6 @@ reset:			CLEAN_START			; ouput: all ram registers 0
                 lda #1
                 sta CTRLPF
 
-                ; lda #0	            ; (2) set D1 to 0 to end VBLANK
-                ; sta c16_1,1
-                ; sta c16_1
-                
                 lda #$1E
                 sta COLUPF          
 
@@ -48,11 +47,7 @@ nextframe:		VERTICAL_SYNC	    ; output: a = 0; 3 scanlines
                 sta TIM64T          ; Set a count of 43 with 64-clock interval
 ; -------- do stuff  -------------------------------
                 ; jsr add
-                ; jsr add
-                ; jsr add
-                ; jsr add
-                jsr inc16_1
-
+                
 ; -------- wait ------------------------------------                                
                 lda INTIM           ; check the timer          
                 bne .-3             ; 2 bytes del opcode (bne) + 1 byte operando                                    
@@ -62,34 +57,41 @@ nextframe:		VERTICAL_SYNC	    ; output: a = 0; 3 scanlines
                 
                 sta VBLANK
                 
-                
                 lda #PF_H
                 sta scanline       ; 1 byte - current scanline
                 
                 
+                jsr inc16
+                ; PF0
                 
-                sta WSYNC           ; primera linea visible
-                lda c16_1,1
-                sta COLUPF
-kernel:		    
-                sta WSYNC           ; no lo cuento en la snl  
+                ; PF1
+                lda c16_1
+                sta plfys+1
                 
-                lda t_                
-                sta PF1
+                ; PF2
+                lda c16_1+1
+                jsr reverseBits
+                sta plfys+2
 
-                lda t_,1                
+                sta WSYNC           ; primera linea visible
+                
+                
+kernel:		    sta WSYNC           ; no lo cuento en la snl  
+                ldy #0
+
+                lda plfys,y
+                sta PF0                
+                iny
+
+
+                lda plfys,y
+                sta PF1                
+                iny
+
+                sleep 10
+                lda plfys,y                
                 sta PF2
 
-                
-                sta WSYNC
-
-                jsr bytebeat2
-                dec scanline
-
-                
-                ; lda c16_1,1
-                ; sta COLUBK          ;(3)
-                
                 dec scanline                 ; (2)
                 bne kernel          ; (3) 2 bytes del opcode (beq) + 1 byte operando + byte del salto
                 
@@ -117,75 +119,67 @@ kernel:
 
 
 
-                ; Shift a 16bit value by one place left (e.g. multiply by two)
-asl16       ASL MEM+0       ;Shift the LSB
-            ROL MEM+1       ;Rotate the MSB
+            ; Shift a 16bit value by one place left (e.g. multiply by two)
+asl16       ASL params+0       ;Shift the LSB
+            ROL params+1       ;Rotate the MSB
             rts
 
 
-
-inc16_1	        clc		
-                lda c16_1,1
+inc16	        clc		
+                lda c16_1+1
                 adc #1
-                sta c16_1,1
+                sta c16_1+1
                 bcc ok             
-                inc c16_1
+                inc c16_1+0
 ok
                 rts
 
 
-bytebeat2       clc
-                lda c16_1,1 
-                ; adc scanline
-                ; bcc next
-                ; inc c16_1
-; next                
-                lsr t_,1    
-                ora t_,1
-                sta t_,1
-                ; asl t_,1
-                ; ora t_,1           
+reverseBits:
+    tax
+    lda reversedOrderBits,x      ; Load the value to be reversed from memory
+    rts              
                 
-                lda c16_1 
-                lsr t_    
-                ora t_
-                sta t_
-                ; asl t_
-                ; ora t_           
-                
-                ; ror t_                
-                
-                inc t_
-                
-                rts
+reversedOrderBits
+            .word $00, $80, $40, $c0, $20, $a0, $60, $e0
+            .word $10, $90, $50, $d0, $30, $b0, $70, $f0
+            .word $08, $88, $48, $c8, $28, $a8, $68, $e8
+            .word $18, $98, $58, $d8, $38, $b8, $78, $f8
+            .word $04, $84, $44, $c4, $24, $a4, $64, $e4
+            .word $14, $94, $54, $d4, $34, $b4, $74, $f4
+            .word $0c, $8c, $4c, $cc, $2c, $ac, $6c, $ec
+            .word $1c, $9c, $5c, $dc, $3c, $bc, $7c, $fc
+            .word $02, $82, $42, $c2, $22, $a2, $62, $e2
+            .word $12, $92, $52, $d2, $32, $b2, $72, $f2
+            .word $0a, $8a, $4a, $ca, $2a, $aa, $6a, $ea
+            .word $1a, $9a, $5a, $da, $3a, $ba, $7a, $fa
+            .word $06, $86, $46, $c6, $26, $a6, $66, $e6
+            .word $16, $96, $56, $d6, $36, $b6, $76, $f6
+            .word $0e, $8e, $4e, $ce, $2e, $ae, $6e, $ee
+            .word $1e, $9e, $5e, $de, $3e, $be, $7e, $fe
+            .word $01, $81, $41, $c1, $21, $a1, $61, $e1
+            .word $11, $91, $51, $d1, $31, $b1, $71, $f1
+            .word $09, $89, $49, $c9, $29, $a9, $69, $e9
+            .word $19, $99, $59, $d9, $39, $b9, $79, $f9
+            .word $05, $85, $45, $c5, $25, $a5, $65, $e5
+            .word $15, $95, $55, $d5, $35, $b5, $75, $f5
+            .word $0d, $8d, $4d, $cd, $2d, $ad, $6d, $ed
+            .word $1d, $9d, $5d, $dd, $3d, $bd, $7d, $fd
+            .word $03, $83, $43, $c3, $23, $a3, $63, $e3
+            .word $13, $93, $53, $d3, $33, $b3, $73, $f3
+            .word $0b, $8b, $4b, $cb, $2b, $ab, $6b, $eb
+            .word $1b, $9b, $5b, $db, $3b, $bb, $7b, $fb
+            .word $07, $87, $47, $c7, $27, $a7, $67, $e7
+            .word $17, $97, $57, $d7, $37, $b7, $77, $f7
+            .word $0f, $8f, $4f, $cf, $2f, $af, $6f, $ef
+            .word $1f, $9f, $5f, $df, $3f, $bf, $7f, $ff    
 
-
-bytebeat
-                lda t_
-                ror
-                ror
-                ora t_
-                ror
-                ror
-                ora t_
-                and #$3F
-                ror
-                ror
-                and t_
-                rts
-
+                
                 org 	$FFFA
 
                 .word reset     	; NMI  ($FFFA)
                 .word reset     	; RESET ($FFFC)
                 .word reset     	; IRQ ($FFFE)
-
-
-
-
-
-
-
 
 
 
